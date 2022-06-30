89ec801700b1af93cc285ac7ff613ffe473f0bc8
anthony4tner <adfortner@protonmail.com>
2022-06-29 18:22:42 -0700

    both working at once

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require(`dotenv`).config()
+require("dotenv").config()
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
@@ -7,55 +7,133 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-    siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteTitle: "Cara",
+    siteTitleAlt: "Cara - Gatsby Starter Portfolio",
+    siteHeadline: "Cara - Gatsby Theme from @lekoarts",
+    siteUrl: "https://cara.lekoarts.de",
+    siteDescription: "Playful and Colorful One-Page portfolio featuring Parallax effects and animations",
+    siteLanguage: "en",
+    siteImage: "/banner.jpg",
+    author: "@lekoarts_de",
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-cara`,
+      resolve: "@lekoarts/gatsby-theme-cara",
       // See the theme's README for all available options
-      options: {},
+      options: {
+        basePath: "/sideproject",
+      },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-        start_url: `/`,
-        background_color: `#141821`,
+        name: "Cara - @lekoarts/gatsby-theme-cara",
+        short_name: "Cara",
+        description: "Playful and Colorful One-Page portfolio featuring Parallax effects and animations",
+        start_url: "/",
+        background_color: "#141821",
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#f6ad55`,
-        display: `standalone`,
+        // theme_color: "#f6ad55",
+        display: "standalone",
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    "gatsby-plugin-gatsby-cloud",
     shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
+        analyzerMode: "static",
+        reportFilename: "_bundle.html",
         openAnalyzer: false,
       },
     },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        sassOptions: {
+          indentedSyntax: true,
+        },
+      },
+    },
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/img`,
+        name: "images",
+      },
+    },
+    `gatsby-plugin-image`,
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              destinationDir: "static",
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+      options: {
+        develop: true, // Activates purging in npm run develop
+        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+      },
+    }, // must be after other CSS plugins
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ].filter(Boolean),
 }
